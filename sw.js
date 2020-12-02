60a0b6fca6c2b776f6c53689fa81a7e182aeb945
Armando233-abc <armandovigliotti5@gmail.com>
2020-12-02 22:29:57 +0100

    sw file

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of assets" service worker
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "pwa-morra-cinese-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
